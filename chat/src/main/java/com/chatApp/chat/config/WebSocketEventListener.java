24d5418da34f20718d80da1578df61c71e02cc0b
162ayush <ayushshivhare162@gmail.com>
2025-03-02 17:39:51 +0530

    Completed WebSocketEventListener implementation

--- chat/src/main/java/com/chatApp/chat/config/WebSocketEventListener.java
+++ chat/src/main/java/com/chatApp/chat/config/WebSocketEventListener.java
@@ -1,26 +1,39 @@
-//package com.chatApp.chat.config;
-//
-//import lombok.RequiredArgsConstructor;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.context.event.EventListener;
-//import org.springframework.stereotype.Component;
-//
-//@Component
-//@RequiredArgsConstructor
-//@Slf4j
-//public class WebSocketEventListener {
-//
-//    @EventListener
-//    public void handleWebSocketDisconnectListener(SessionDisconnectEvent event) {
-////        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
-////        String username = (String) headerAccessor.getSessionAttributes().get("username");
-////        if (username != null) {
-////            log.info("user disconnected: {}", username);
-////            var chatMessage = ChatMessage.builder()
-////                    .type(MessageType.LEAVE)
-////                    .sender(username)
-////                    .build();
-////            messagingTemplate.convertAndSend("/topic/public", chatMessage);
-////        }
-//    }
-//}
+package com.chatApp.chat.config;
+
+import com.chatApp.chat.model.Status;
+import org.apache.logging.log4j.message.Message;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.stereotype.Component;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+import java.util.Objects;
+import java.time.LocalDateTime;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class WebSocketEventListener {
+
+    private final SimpMessagingTemplate messagingTemplate;
+
+    public void handleUserDisconnect(SessionDisconnectEvent event) {
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+        String username = (String) headerAccessor.getSessionAttributes().get("username");
+
+        if (Objects.nonNull(username)) {
+            log.info("User disconnected: {}", username);
+
+            Message chatMessage = Message.builder()
+                    .senderName(username)
+                    .message("User has left the chat")
+                    .timestamp(LocalDateTime.now())
+                    .status(Status.LEAVE)
+                    .build();
+
+            messagingTemplate.convertAndSend("/topic/public", chatMessage);
+        }
+    }
+}
